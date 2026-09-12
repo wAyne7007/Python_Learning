fb653776e8e744e2582d520c513670d90a055442
wAyne7007 <freke.pasha@gmail.com>
2026-09-13 02:37:15 +0500

    Завершил модуль 6.2 строки

---
+++ stepikday10_len_str.py
@@ -0,0 +1,83 @@
+# 6.2 Строковый тип данных
+
+
+# Задача 1
+# s1 = '"Python is a great language!", said Fred. "I don'
+# s2 = "'"
+# s3 = 't ever remember having this much fun before."'
+
+# ss = s1 + s2 + s3
+# print(ss)
+
+
+# Задача 2
+# name = input()
+# surname = input()
+# s1 = surname + '!'
+# print("Hello", name, s1, "You have just delved into Python")
+
+
+# Задача 3
+# name = input()
+# lenght1 = len(name)
+# print("Футбольная команда", name, "имеет длину", lenght1, "символов")
+
+
+# Задача 4
+# city1 = input()
+# city2 = input()
+# city3 = input()
+# lencity1 = len(city1)
+# lencity2 = len(city2)
+# lencity3 = len(city3)
+# maxcity = max(lencity1, lencity2, lencity3)
+# mincity = min(lencity1, lencity2, lencity3)
+# cityfirst = ''
+# citysecond = ''
+# if mincity == lencity1:
+#     cityfirst = city1
+# elif mincity == lencity2:
+#     cityfirst = city2
+# else:
+#     cityfirst = city3
+# if maxcity == lencity1:
+#     citysecond = city1
+# elif maxcity == lencity2:
+#     citysecond = city2
+# else:
+#     citysecond = city3
+# print(cityfirst, citysecond, sep='\n')
+
+
+# Задача 5
+# a = len(input())
+# b = len(input())
+# c = len(input())
+# if (2 * b - c - a) * (2 * c - b - a) * (2 * a - b - c) == 0:
+#     print("YES")
+# else:
+#     print("NO")
+
+
+# Задача 6
+# s1 = input()
+# if 'синий' in s1:
+#     print("YES")
+# else:
+#     print("NO")
+
+
+# Задача 7
+# s1 = input()
+# if "суббота" in s1 or "воскресенье" in s1:
+#     print("YES")
+# else:
+#     print("NO")
+
+
+# Задача 8 
+# email = input()
+# if '.' in email and '@' in email:
+#     print("YES")
+# else:
+#     print("NO")
